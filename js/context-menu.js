14a6c203f5c7cf6d2cd73a0428479bb1d46f3718
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2019-10-31 08:29:14 -0400

    Update context-menu.js

--- js/context-menu.js
+++ js/context-menu.js
@@ -1,23 +1,50 @@
-$(document).bind("contextmenu",function(e){
-  e.preventDefault();
-  console.log(e.pageX + "," + e.pageY);
-  $("#cntnr").css("left",e.pageX);
-  $("#cntnr").css("top",e.pageY);
- // $("#cntnr").hide(100);        
-  $("#cntnr").fadeIn(200,startFocusOut());      
+$(document).bind("contextmenu", function (e) {
+	e.preventDefault();
+	console.log(e.pageX + "," + e.pageY);
+	$("#cntnr").css("left", e.pageX);
+	$("#cntnr").css("top", e.pageY);
+	// $("#cntnr").hide(100);        
+	$("#cntnr").fadeIn(200, startFocusOut());
 });
 
-
-function startFocusOut(){
-  $(document).on("click",function(){
-  $("#cntnr").hide();        
-  $(document).off("click");
-  });
+function startFocusOut() {
+	$(document).on("click", function () {
+		$("#cntnr").hide();
+		$(document).off("click");
+	});
 }
 
-document.getElementById("cut").onclick = function () { document.execCommand("cut")};
+function selectElementText(el, win) {
+	win = win || window;
+	var doc = win.document,
+		sel, range;
+	if (win.getSelection && doc.createRange) {
+		sel = win.getSelection();
+		range = doc.createRange();
+		range.selectNodeContents(el);
+		sel.removeAllRanges();
+		sel.addRange(range);
+	} else if (doc.body.createTextRange) {
+		range = doc.body.createTextRange();
+		range.moveToElementText(el);
+		range.select();
+	}
+}
 
-document.getElementById("copy").onclick = function () { document.execCommand("copy")};
 
-document.getElementById("paste").onclick = function () { document.execCommand("paste")};
+document.getElementById("cut").onclick = function () {
+	document.execCommand("cut");
+};
 
+document.getElementById("copy").onclick = function () {
+	document.execCommand("copy");
+};
+
+document.getElementById("paste").onclick = function () {
+	document.execCommand("paste");
+};
+
+document.getElementById("select").onclick = function () {
+	selectElementText(document.getElementById("page"));
+	selectElementText(elementInIframe, iframe.contentWindow);
+};
