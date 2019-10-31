33619c7ff851cdbbd8dc75505fd0c64c2a3fd7ea
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2019-10-31 08:43:58 -0400

    Update context-menu.js

--- js/context-menu.js
+++ js/context-menu.js
@@ -1,3 +1,8 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 $(document).bind("contextmenu", function (e) {
 	e.preventDefault();
 	console.log(e.pageX + "," + e.pageY);
