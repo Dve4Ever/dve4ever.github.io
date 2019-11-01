f1f4f70b2b5c13c670215337a9dbb3998bda43c8
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2019-11-01 08:02:39 -0400

    Update context-menu.js

--- js/context-menu.js
+++ js/context-menu.js
@@ -3,53 +3,64 @@ script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$(document).bind("contextmenu", function (e) {
-	e.preventDefault();
-	console.log(e.pageX + "," + e.pageY);
-	$("#cntnr").css("left", e.pageX);
-	$("#cntnr").css("top", e.pageY);
-	// $("#cntnr").hide(100);        
-	$("#cntnr").fadeIn(200, startFocusOut());
+$(document).bind("contextmenu", function(e) {
+  e.preventDefault();
+  console.log(e.pageX + "," + e.pageY);
+  $("#cntnr").css("left", e.pageX);
+  $("#cntnr").css("top", e.pageY);
+  // $("#cntnr").hide(100);        
+  $("#cntnr").fadeIn(200, startFocusOut());
 });
 
 function startFocusOut() {
-	$(document).on("click", function () {
-		$("#cntnr").hide();
-		$(document).off("click");
-	});
+  $(document).on("click", function() {
+    $("#cntnr").hide();
+    $(document).off("click");
+  });
 }
 
 function selectElementText(el, win) {
-	win = win || window;
-	var doc = win.document,
-		sel, range;
-	if (win.getSelection && doc.createRange) {
-		sel = win.getSelection();
-		range = doc.createRange();
-		range.selectNodeContents(el);
-		sel.removeAllRanges();
-		sel.addRange(range);
-	} else if (doc.body.createTextRange) {
-		range = doc.body.createTextRange();
-		range.moveToElementText(el);
-		range.select();
-	}
+  win = win || window;
+  var doc = win.document,
+    sel, range;
+  if (win.getSelection && doc.createRange) {
+    sel = win.getSelection();
+    range = doc.createRange();
+    range.selectNodeContents(el);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  } else if (doc.body.createTextRange) {
+    range = doc.body.createTextRange();
+    range.moveToElementText(el);
+    range.select();
+  }
 }
 
-
-document.getElementById("cut").onclick = function () {
-	document.execCommand("cut");
+document.getElementById("back").onclick = function() {
+  window.history.back();
 };
 
-document.getElementById("copy").onclick = function () {
-	document.execCommand("copy");
+document.getElementById("foward").onclick = function() {
+  window.history.forward();
 };
 
-document.getElementById("paste").onclick = function () {
-	document.execCommand("paste");
+document.getElementById("reload").onclick = function() {
+  location.reload();
 };
 
-document.getElementById("select").onclick = function () {
-	selectElementText(document.getElementById("page"));
-	selectElementText(elementInIframe, iframe.contentWindow);
+document.getElementById("cut").onclick = function() {
+  document.execCommand("cut");
+};
+
+document.getElementById("copy").onclick = function() {
+  document.execCommand("copy");
+};
+
+document.getElementById("paste").onclick = function() {
+  document.execCommand("paste");
+};
+
+document.getElementById("select").onclick = function() {
+  selectElementText(document.getElementById("page"));
+  selectElementText(elementInIframe, iframe.contentWindow);
 };
