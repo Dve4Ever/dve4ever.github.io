00b56f2c5b5a700dd33d1d45504989bb48a36808
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2019-12-10 08:31:16 -0500

    Update search.js

--- js/home/search.js
+++ js/home/search.js
@@ -1,6 +1,1 @@
-function getInputValue() {
-  // Selecting the input element and get its value 
-  var inputVal = "https://dve4ever.github.io/search?q=" + document.getElementById("myInput").value;
-  // Displaying the value
-  window.open(inputVal)
-}
+function getInputValue(){var e="https://dve4ever.github.io/search?q="+document.getElementById("myInput").value;window.open(e)}
